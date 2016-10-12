f9e69efbbc578222f0b8c623893a17c2f4e033a2
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-10-12 12:03:50 -0300

    Redefine caso de teste do Problema do Troco (ótima)

--- src/test/java/br/ufg/inf/es/tacs20162/greedy/TrocoTest.java
+++ src/test/java/br/ufg/inf/es/tacs20162/greedy/TrocoTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.Arrays;
 
+import br.ufg.inf.es.tacs20162.greedy.ProblemaDoTroco.Troco;
+
 /**
  *
  * @author gustavosotnas
@@ -13,19 +15,30 @@ public class TrocoTest {
     /**
      * Caso de teste do algoritmo do Problema do Troco que dá uma solução ótima.
      */
-/*    @Test
+    @Test
     public void testCalculaTrocoOtimo() {
 
         System.out.println("Problema do Troco com solução ótima");
 
         double conta = 24.00; // conta a ser paga
         double pago = 50.00; // dinheiro pago pelo cliente
+        int[] notasDisponiveis = {1, 2, 5, 10, 20}; // reais
+        int[] moedasDisponiveis = {5, 10, 25, 50, 100}; // centavos
 
-        //String expResult = "";
-        String result = ProblemaDoTroco.calculaTroco(conta, pago);
-        //assertEquals(expResult, result);
-        System.out.println(result);
-    }*/
+        Troco expResult = new Troco();
+        expResult.troco = 26.00;   // Troco de R$ 26,00 
+        expResult.notas.put(20, 1);// 1 nota de R$ 20,00
+        expResult.notas.put(5, 1); // 1 nota de R$ 5,00
+        expResult.notas.put(1, 1); // 1 nota de R$ 1,00
+        expResult.moedas.put(100, 0);
+        expResult.moedas.put(50, 0);
+        expResult.moedas.put(25, 0);
+        expResult.moedas.put(10, 0);
+        expResult.moedas.put(5, 0); // troco sem moedas
+
+        Troco result = ProblemaDoTroco.calculaTroco(notasDisponiveis, moedasDisponiveis, conta, pago);
+        assertEquals(expResult, result);
+    }
 
 	/**
 	 * Teste da função ProblemaDoTroco.sortReverse(int[] vector)
