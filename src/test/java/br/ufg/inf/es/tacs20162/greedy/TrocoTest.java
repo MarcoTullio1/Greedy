52f38fe88d83df347ccb6de4f8dd9286ab6daa90
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-10-12 14:09:45 -0300

    Implementa caso de teste do Problema do Troco que NÃO dá uma solução ótima
    Fonte: https://www.youtube.com/watch?v=HA_Lf-ORE34

--- src/test/java/br/ufg/inf/es/tacs20162/greedy/TrocoTest.java
+++ src/test/java/br/ufg/inf/es/tacs20162/greedy/TrocoTest.java
@@ -44,4 +44,34 @@ public class TrocoTest {
 
         assertEquals(expResult.toString(), result.toString());
     }
+
+    /**
+     * Caso de teste do algoritmo do Problema do Troco que NÃO dá uma solução ótima.
+     */
+    @Test
+    public void testCalculaTroco() {
+
+        System.out.println("=================\n"
+        				 + "Problema do Troco\n"
+        				 + "=================\n");
+
+        double conta = 12.00; // conta a ser paga
+        double pago = 20.00; // dinheiro pago pelo cliente
+        int[] notasDisponiveis = {1, 4, 6}; // reais
+        int[] moedasDisponiveis = {5, 10, 25, 50, 100}; // centavos
+
+        Troco expResult = new Troco();
+        expResult.troco = 8.00;   // Troco de R$ 8,00 
+        expResult.notas.put(4, 2); // 2 notas de R$ 4,00
+        System.out.println("Expected output:\n"
+				 + "----------------\n");
+        System.out.println(expResult.toString());
+
+        Troco result = ProblemaDoTroco.calculaTroco(notasDisponiveis, moedasDisponiveis, conta, pago);
+        System.out.println("Output:\n"
+				 + "-------\n");
+        System.out.println(result.toString() + "\n");
+
+        assertEquals(expResult.toString(), result.toString());
+    }
 }
